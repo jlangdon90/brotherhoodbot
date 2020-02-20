2c738aa5c781725f37d5dd2fa7e5b7fc678b935e
Jared Langdon <jlangdon0523@gmail.com>
2020-02-19 23:44:08 -0800

    add in postgres connection

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const {token, prefix} = require('./config.json');
+//local config options
+const {token, prefix, databaseUrl} = require('./config.json');
+const connectionString = process.env.DATABASE_URL || databaseUrl
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+//Discord bot setup
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+//PG database setup
+const { Pool, Client } = require('pg');
+const dbClient = new Client({
+    connectionString: connectionString
+})
+dbClient.connect();
+dbClient.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    dbClient.end()
+})
+
+
+bot.once('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}`);
 });
 
-client.on('message', msg => {
+bot.on('message', msg => {
     if (msg.content === prefix + 'ping') {
         msg.reply('Pong!');
         console.log('Replied!')
     }
 });
 
-client.login(process.env.DISCORD_TOKEN || token)
-console.log(process.env.DISCORD_TOKEN)
+bot.login(process.env.DISCORD_TOKEN || token)
